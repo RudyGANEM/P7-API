abdbaadc00332844b86b42872bd9d58b1276257e
RudyGANEM <rudy.ganem@free.fr>
2024-09-08 15:16:19 +0200

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ import os
 # Ajoute le répertoire parent au PYTHONPATH
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from main_back_end import app
+from main import app
 
 from fastapi.testclient import TestClient
 
